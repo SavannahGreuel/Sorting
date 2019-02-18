249fb69c72070b21d72cf52d8a130343bd721540
SavannahGreuel <smallidgesavannah@gmail.com>
2019-02-18 13:01:23 -0700

    linear search complete

--- project/searching.py
+++ project/searching.py
@@ -2,8 +2,25 @@
 def linear_search(arr, target):
   
   # TO-DO: add missing code
+  
+# Linearly search x in arr[] 
+# If x is present then return its location 
+# else return -1 
+  for i in range(len(arr)):
+    if arr[i] == target:
+      return i
 
-   return -1   # not found
+  return -1   # not found
+arr = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170]
+target = 110
+
+print(arr)
+result = linear_search(arr,target)
+if(result == -1): 
+    print("Element is not present in array") 
+else: 
+    print("Element is present at index", result); 
+
 
 
 # STRETCH: write an iterative implementation of Binary Search 
