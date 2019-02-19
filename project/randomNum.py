954eaa0cf19d487e8c3345792fe9f009ac9a6268
SavannahGreuel <smallidgesavannah@gmail.com>
2019-02-19 13:09:46 -0700

    complete quick_sort.py

---
+++ project/randomNum.py
@@ -0,0 +1,7 @@
+import random
+randList = []
+for i in range(0,10):
+    x = random.randint( 1,100)
+    randList.append(x)
+
+print(randList)
--- project/recursive_sorting.py
+++ project/recursive_sorting.py
@@ -44,9 +44,33 @@ def merge_sort_in_place(arr, l, r):
 
 # TO-DO: implement the Quick Sort function below USING RECURSION
 def quick_sort( arr, low, high ):
+    i = (low -1) # index of smaller element
+    pivot = arr[high] # pivot (last element as pivot)
 
-    return arr
+    for j in range( low, high):
+        if arr[j] <= pivot:  # If current element is smaller than or equal to pivot 
+            i = i + 1 # increment index of smaller element 
+            arr[i], arr[j] = arr[j], arr[i]
 
+    arr[i+1],arr[high] = arr[high],arr[i+1] 
+    return ( i + 1)
+
+
+def run_quick_sort ( arr, low, high ):
+    if low < high :
+        qs = quick_sort(arr,low,high)
+
+        run_quick_sort(arr, low, qs - 1)
+        run_quick_sort(arr, qs + 1, high)
+
+        return arr
+
+# arr = [75, 29, 4, 47, 24, 67, 12, 50, 63, 96]
+arr = [66, 84, 4, 57, 42, 77, 7, 59, 95, 26]
+n = len(arr)
+
+result = run_quick_sort( arr, 0, n - 1)
+print (f'{result}')
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
